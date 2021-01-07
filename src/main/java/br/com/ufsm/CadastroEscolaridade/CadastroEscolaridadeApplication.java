c1523a1cc0e6d96eaecbffcf2826b3e8323ec76f
Pedro Carara <cararax@gmail.com>
2021-01-07 14:23:26 -0300

    Commit inicial

---
+++ src/main/java/br/com/ufsm/CadastroEscolaridade/CadastroEscolaridadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufsm.CadastroEscolaridade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEscolaridadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroEscolaridadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ufsm/CadastroEscolaridade/CadastroEscolaridadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufsm.CadastroEscolaridade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroEscolaridadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
